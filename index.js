9f8e9260d39b7483c5b7be23f0735a75fa29a6c5
mateo <mateoloavvio12@gmail.com>
2023-02-13 20:48:38 -0500

    se crea ejemplo funcion flecha

---
+++ index.js
@@ -0,0 +1,8 @@
+let juagador = "James Rodriguez"
+
+if (5>2){
+    let jugador = "Lucho Diaz"
+}
+
+console.log("Hola "+juagador)
+
---
+++ punto1Flecha.js
@@ -0,0 +1,14 @@
+let calcularDistancia = (x1, y1, x2, y2) => {
+    let restax = x2 - x1
+    let restay = y2 - y1
+    let potenciax = Math.pow(restax, 2)
+    let potenciay = Math.pow(restay, 2)
+    let sumatoria = potenciax + potenciay
+
+    let raizCuadrada = Math.sqrt(sumatoria)
+
+    return raizCuadrada
+}
+let distanciaCalculada = calcularDistancia(1, 1, 3, 3)
+
+console.log(`La distancia es de ${distanciaCalculada}`)
